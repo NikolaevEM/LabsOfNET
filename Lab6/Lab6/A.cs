2a1677effcfab86ce67b8921f6f3c19b3d9b7105
NikolaevEM <43157756+NikolaevEM@users.noreply.github.com>
2020-03-13 13:24:18 +0900

    Add files via upload

---
+++ Lab6/Lab6/A.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class A
+    {
+        public int start()
+        {
+            String num;
+            int sum = 0;
+
+            num = Console.ReadLine();
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine(num[i]);
+                sum += Convert.ToInt16(num[i]-'0');
+            }
+
+            Console.WriteLine("Сумма цифр = " + Convert.ToString(sum));
+
+            return sum;
+        }
+
+    }
+}
---
+++ Lab6/Lab6/B.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab6
+{
+    class B
+    {
+
+        public string revers(string s)
+        {
+            for (int i = s.Length; i > 0; i--)
+            {
+
+            }
+        }
+
+        public string start()
+        {
+            string[] predlogs;
+            StreamReader sr = new StreamReader("predlogs.txt",Encoding.GetEncoding(866));
+            predlogs = sr.ReadToEnd().Split('\n');
+
+            string[] s = Console.ReadLine().Split(' ');
+            bool chet = false;
+
+            foreach (string i in s)
+            {
+                if (!predlogs.Contains(i))
+                {
+                    if (chet)
+                    {
+                        Console.Write(revers(i) + ' ');
+                        chet = false;
+                    }
+                    else
+                    {
+                        Console.Write(i + ' ');
+                        chet = true;
+                    }
+                } 
+            }
+
+
+
+            return "1";
+        }
+    }
+}
